46cbd95196845b55030e6101adae69d260f352ad
Sajal Jain <jsajal1993@gmail.com>
2019-12-04 21:58:05 -0600

    Added code to set up flask server

--- server.py
+++ server.py
@@ -1,12 +1,24 @@
 from flask import Flask, request
+import sqlite3
+import os, json
+from queryTranslate import decryptQueryData
+from queryExecute import prepare_data
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def root():
     print(request.json)
+    data = request.json
+    rootNode = data.get("root")
+    finalQuery = decryptQueryData(data, rootNode)
+    #print("final query = ", finalQuery)
 
-    return "hi"
+    cur = prepare_data()
+    cur.execute(finalQuery)
+    results = cur.fetchall()
+
+    return " ".join(map(str, results))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
